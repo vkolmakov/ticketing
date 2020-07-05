f759bb6cefadec144172834806b25aa7d584ebcc
vkolmakov <vkolmakov256@gmail.com>
2020-07-04 20:23:28 -0500

    Add basic tests with supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors"; // allows error handling inside async functions without having to rely on the `next` callback
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+
+import NotFoundError from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true); // since we proxy the traffic through nginx ingress we need to tell express to allow that
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false, // don't encrypt the cookie itself
+		secure: true, // only allow on https
+	})
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// 404s
+app.use("*", () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from "express";
-import "express-async-errors"; // allows error handling inside async functions without having to rely on the `next` callback
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-
-import NotFoundError from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); // since we proxy the traffic through nginx ingress we need to tell express to allow that
-app.use(json());
-app.use(
-	cookieSession({
-		signed: false, // don't encrypt the cookie itself
-		secure: true, // only allow on https
-	})
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// 404s
-app.use("*", () => {
-	throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 async function start() {
 	// Better to fail as soon as possible in case we forgot to set the environment variable
