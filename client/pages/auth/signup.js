b8c5a30000546257c527211caaea409fc0a2bf75
vkolmakov <vkolmakov256@gmail.com>
2020-07-08 21:13:18 -0500

    Refactor request+errors logic into a hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 export default () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [errors, setErrors] = useState([]);
+	const { doRequest, errors } = useRequest({
+		url: "/api/users/signup",
+		method: "post",
+		body: { email, password },
+	});
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
-
-		try {
-			const response = await axios.post("/api/users/signup", {
-				email,
-				password,
-			});
-
-			console.log(response.data);
-		} catch (err) {
-			setErrors(err.response.data.errors);
-		}
+		await doRequest();
 	};
 
 	return (
@@ -47,16 +42,7 @@ export default () => {
 				</label>
 			</div>
 
-			{errors.length > 0 && (
-				<div className="alert alert-danger">
-					<h4>Oooops...</h4>
-					<ul className="my-0">
-						{errors.map((err) => (
-							<li key={err.message}>{err.message}</li>
-						))}
-					</ul>
-				</div>
-			)}
+			{errors}
 
 			<button onClick={onSubmit} className="btn btn-primary">
 				Sign up
