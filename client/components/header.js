1f73462f5fd527dd9339cb2022994b463a31ce37
vkolmakov <vkolmakov256@gmail.com>
2020-07-09 21:41:43 -0500

    Add navbar

---
+++ client/components/header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default ({ currentUser }) => {
+	const links = [
+		!currentUser && { label: "Sign up", href: "/auth/signup" },
+		!currentUser && { label: "Sign in", href: "/auth/signin" },
+		currentUser && { label: "Sign out", href: "/auth/signout" },
+	];
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<Link href="/">
+				<a className="navbar-brand">GitTix</a>
+			</Link>
+
+			<div className="d-flex justify-content-end">
+				<ul className="nav d-flex align-items-center">
+					{links.filter(Boolean).map(({ label, href }) => {
+						return (
+							<li className="nav-item" key={href}>
+								<Link href={href}>
+									<a className="nav-link">{label}</a>
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</nav>
+	);
+};
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
 	return (
 		<div>
-			<h1>Header. {currentUser.email}</h1>
-			<Component {...pageProps}></Component>;
+			<Header currentUser={currentUser}></Header>
+			<Component {...pageProps}></Component>
 		</div>
 	);
 };
