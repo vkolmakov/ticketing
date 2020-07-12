b8bbb8317eb06422086ba78d2d936226ec4c5c40
vkolmakov <vkolmakov256@gmail.com>
2020-07-11 20:51:13 -0500

    Add auth for the create route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors"; // allows error handling inside async functions without having to rely on the `next` callback
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@tickets-vk/common";
+import { errorHandler, NotFoundError, currentUser } from "@tickets-vk/common";
 import { createTicketRouter } from "./routes/new";
 
 const app = express();
@@ -14,6 +14,9 @@ app.use(
 		secure: process.env.NODE_ENV !== "test", // only allow on https unless testing
 	})
 );
+
+// must be after the cookieSession since cookie contains the information
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
