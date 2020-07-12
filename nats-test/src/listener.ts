983887182ddf70321315f6250ed3a70b1188f039
vkolmakov <vkolmakov256@gmail.com>
2020-07-12 13:05:52 -0500

    NATS_TEST - add manual ack and clean up when process was killed

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,18 @@ const stan = nats.connect("ticketing", listenerId, {
 
 stan.on("connect", () => {
 	console.log("Listener connected to NATS");
+
+	stan.on("close", () => {
+		console.log("NATS connection closed!");
+		process.exit();
+	});
+
+	const options = stan.subscriptionOptions().setManualAckMode(true);
+
 	const subscription = stan.subscribe(
 		"ticket:created",
-		"orders-service-queue-group"
+		"orders-service-queue-group",
+		options
 	);
 
 	subscription.on("message", (msg: Message) => {
@@ -27,5 +36,10 @@ stan.on("connect", () => {
 				`Received event #${msg.getSequence()}, with data: ${dataObject}`
 			);
 		}
+
+		msg.ack();
 	});
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
