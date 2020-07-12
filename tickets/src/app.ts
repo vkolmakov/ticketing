1f1b36c5a50aa8bf1e06f8c23a8775bfff614a47
vkolmakov <vkolmakov256@gmail.com>
2020-07-11 20:45:15 -0500

    Start TDD on a create route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import "express-async-errors"; // allows error handling inside async functions without having to rely on the `next` callback
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-
 import { errorHandler, NotFoundError } from "@tickets-vk/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true); // since we proxy the traffic through nginx ingress we need to tell express to allow that
@@ -14,6 +14,8 @@ app.use(
 		secure: process.env.NODE_ENV !== "test", // only allow on https unless testing
 	})
 );
+
+app.use(createTicketRouter);
 
 // 404s
 app.use("*", () => {
