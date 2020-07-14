5fa4d2f47b2e63539894403ef14762fe3cdb658a
vkolmakov <vkolmakov256@gmail.com>
2020-07-13 21:27:09 -0500

    NATS-TEST outline a Listener helper class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -16,34 +16,66 @@ stan.on("connect", () => {
 		process.exit();
 	});
 
-	const options = stan
-		.subscriptionOptions()
-		.setManualAckMode(true)
-		.setDeliverAllAvailable() // gives us ALL of the events that ever happened before
-		.setDurableName("accounting-service"); // sets a persistent name - if we disconnect for a while and reconnect, NATS will redeliver only the missed messages. As long as the group name is set, the subscription name will be kept by NATS even if there are no more listeners left.
-
-	const subscription = stan.subscribe(
-		"ticket:created",
-		"queue-group-name",
-		options
-	);
-
-	subscription.on("message", (msg: Message) => {
-		// Every message has a Sequence and Data
-		// Sequence is the message ID on the given topic
-		// Data is the actual data of the message
-		const data = msg.getData();
-
-		if (typeof data === "string") {
-			const dataObject = JSON.parse(data);
-			console.log(
-				`Received event #${msg.getSequence()}, with data: ${data}`
-			);
-		}
-
-		msg.ack();
-	});
+	new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+	abstract subject: string;
+	abstract queueGroupName: string;
+	abstract onMessage(data: any, message: Message): void;
+
+	private client: Stan;
+	protected ackWait = 5 * 1000; // 5 seconds
+
+	constructor(client: Stan) {
+		this.client = client;
+	}
+
+	subscriptionOptions() {
+		return this.client
+			.subscriptionOptions()
+			.setDeliverAllAvailable()
+			.setManualAckMode(true)
+			.setAckWait(this.ackWait)
+			.setDurableName(this.queueGroupName); // it's fine to use the same name as the queue name here
+	}
+
+	listen() {
+		const subscription = this.client.subscribe(
+			this.subject,
+			this.queueGroupName,
+			this.subscriptionOptions()
+		);
+
+		subscription.on("message", (msg: Message) => {
+			console.log(
+				`Message received: ${this.subject} / ${this.queueGroupName}`
+			);
+
+			const parsedData = this.parseMessage(msg);
+			this.onMessage(parsedData, msg);
+		});
+	}
+
+	parseMessage(msg: Message) {
+		const data = msg.getData();
+
+		return typeof data === "string"
+			? JSON.parse(data)
+			: JSON.parse(data.toString("utf-8"));
+	}
+}
+
+class TicketCreatedListener extends Listener {
+	subject = "ticket:created";
+	queueGroupName = "payments-service";
+
+	onMessage(data: any, message: Message): void {
+		console.log("Event data!", data);
+
+		message.ack();
+	}
+}
