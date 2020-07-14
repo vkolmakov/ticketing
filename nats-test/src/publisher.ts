ce505f345cb4b54b336e9390ee51add2d9eebc40
vkolmakov <vkolmakov256@gmail.com>
2020-07-13 22:00:10 -0500

    NATS-TEST - promisify publish method

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,26 +8,13 @@ const stan = nats.connect("ticketing", "abc", {
 	url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
 	console.log("Publisher connected to NATS");
 
 	const publisher = new TicketCreatedPublisher(stan);
-	publisher.publish({
+	await publisher.publish({
 		id: "123",
 		title: "concert",
 		price: 20,
 	});
-
-	// const data = {
-	// 	id: "123",
-	// 	title: "concert",
-	// 	price: 20,
-	// };
-
-	// // NATS only allows sending strings - so we have to serialize it
-	// const dataJsonString = JSON.stringify(data);
-
-	// stan.publish("ticket:created", dataJsonString, () => {
-	// 	console.log("Event published");
-	// });
 });
