895055fc181b77ea1b2493758fd88e07052416c2
vkolmakov <vkolmakov256@gmail.com>
2020-07-23 21:42:31 -0500

    Add initial implementation for ticket-created-listener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,20 @@
+import { TicketCreatedEvent, Listener, Subjects } from "@tickets-vk/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	subject: Subjects.TicketCreated = Subjects.TicketCreated;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: TicketCreatedEvent["data"], message: Message) {
+		const ticket = Ticket.build({
+			title: data.title,
+			price: data.price,
+		});
+
+		await ticket.save();
+
+		message.ack();
+	}
+}
