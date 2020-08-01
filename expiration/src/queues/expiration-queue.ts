bd8f47a2968192ab592c0fd72b54ce0b237a649c
vkolmakov <vkolmakov@iMac.local>
2020-08-01 18:43:46 -0500

    Start publishing expiration:complete events

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 interface Payload {
 	orderId: string;
@@ -12,10 +14,9 @@ const expirationQueue = new Queue<Payload>("order:expiration", {
 
 expirationQueue.process(async (job) => {
 	const { orderId } = job.data;
-	console.log(
-		"I want to publish an expiration:complete event for orderId",
-		orderId
-	);
+	new ExpirationCompletePublisher(natsWrapper.client).publish({
+		orderId,
+	});
 });
 
 export { expirationQueue };
